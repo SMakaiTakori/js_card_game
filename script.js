c20ec151f9576c5f1fc5b216547162beca44e9ae
SMakaiTakori <makait87@icloud.com>
2021-03-03 21:32:08 -0500

    set up clean up fn so cards get cleared

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import Deck from "./deck.js";
 
 const computerCardSlot = document.querySelector(".computer-card-slot");
+const playerCardSlot = document.querySelector(".player-card-slot");
+const computerDeckElement = document.querySelector(".computer-deck");
+const playerDeckElement = document.querySelector(".player-deck");
+const text = document.querySelector(".text");
 
 let playerDeck, computerDeck;
 
@@ -18,9 +22,20 @@ const startGame = () => {
   playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
   computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards));
 
-  console.log(playerDeck);
-  console.log(computerDeck);
+  cleanBeforeRound();
 };
 
 startGame();
 // computerCardSlot.appendChild(deck.cards[0].getHTML());
+const cleanBeforeRound = () => {
+  computerCardSlot.innerHTML = "";
+  playerCardSlot.innerHTML = "";
+  text.innerText = "";
+
+  updateDeckCount();
+};
+
+const updateDeckCount = () => {
+  computerDeckElement.innerText = computerDeck.numberOfCards;
+  playerDeckElement.innerText = playerDeck.numberOfCards;
+};
