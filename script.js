2a75f3f335e1b695f09f21dac3cfc8fe157a6079
SMakaiTakori <makait87@icloud.com>
2021-03-03 21:26:36 -0500

    set up startGame fn to shuffle and create new decks for computer and player

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ import Deck from "./deck.js";
 
 const computerCardSlot = document.querySelector(".computer-card-slot");
 
-const deck = new Deck();
-deck.shuffle();
-console.log(deck.cards);
+let playerDeck, computerDeck;
 
-computerCardSlot.appendChild(deck.cards[0].getHTML());
+// start game immediately
+
+const startGame = () => {
+  // create a new deck
+  // shuffle cards so they are random
+  const deck = new Deck();
+  deck.shuffle();
+
+  // split into two equal piles of cards
+  const deckMidpoint = Math.ceil(deck.numberOfCards / 2);
+  // player should get the first 26 cards
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
+  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards));
+
+  console.log(playerDeck);
+  console.log(computerDeck);
+};
+
+startGame();
+// computerCardSlot.appendChild(deck.cards[0].getHTML());
